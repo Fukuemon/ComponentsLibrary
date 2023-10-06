9758f5dc3358f2cbbcbc6f6d388e50d622b03d52
Fukuemon <Fukuemony029@gmail.com>
2023-10-06 22:04:44 +0900

    ✨ TSのLinterがちゃんと動作するかを確認  #1

--- src/app/Sample.tsx
+++ src/app/Sample.tsx
@@ -1,8 +1,36 @@
 import { FC } from "react";
 
 type SampleProps = {
+  user: {
+    name: string;
+  };
+};
+
+export const Sample: FC<SampleProps> = ({ user }) => {
+  return <div>{user.age}</div>;
+};
+
+type User = {
   name: string;
 };
-export const Sample: FC = ({ user }) => {
-  return <div>{user.name}</div>;
+
+export const Sample2 = () => {
+  const user: User = {
+    name: "John",
+    age: 25,
+  };
+  return <div>{user.age}</div>;
+};
+
+export function getUser(): User {
+  const user: any = {
+    name: "John",
+    age: 25,
+  };
+  return user; // user型はUser型ではなくany型
+}
+
+export const Sample3 = () => {
+  const user = getUser();
+  return <div>{user.age}</div>;
 };
