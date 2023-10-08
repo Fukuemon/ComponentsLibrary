8cfa2085d265d6c2cc841206c3316feacb844e7a
Fukuemon <Fukuemony029@gmail.com>
2023-10-08 15:03:28 +0900

    🔧 tailwindcssのメディアクエリをカスタマイズ #4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,19 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      screens: {
+        sm: '480px',
+        // => @media (min-width: 480px) { ... }
+
+        md: '600px',
+        // => @media (min-width: 600px) { ... }
+
+        lg: '960px',
+        // => @media (min-width: 960px) { ... }
+
+        xl: '1280px'
+        // => @media (min-width: 1280px) { ... }
       }
     }
   },
