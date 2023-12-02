217ae2f90d7d24cc41465419fe1bf69126822ef4
Fukuemon <Fukuemony029@gmail.com>
2023-12-02 12:11:01 +0900

    💄 style: #3スタイルの変更

--- src/app/_components/Tailwind/Button/index.tsx
+++ src/app/_components/Tailwind/Button/index.tsx
@@ -10,8 +10,8 @@ export const Button: FC<ButtonProps> = ({ label, primary = false, ...args }) => 
   return (
     <button
       className={clsx(
-        'bg-blue-300 cursor-pointer rounded-md p-4 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
-        primary && 'bg-primary text-primary-foreground shadow hover:bg-primary/90'
+        'bg-blue-300 cursor-pointer rounded-md p-4 text-sm font-medium text-white shadow hover:bg-blue-400/90',
+        primary && 'bg-red-300 text-primary-foreground shadow hover:bg-primary/90'
       )}
       {...args}
     >
