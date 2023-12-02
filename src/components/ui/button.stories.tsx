4d13545a1c4f0f672cf683335d4ab8f251a8430e
Fukuemon <Fukuemony029@gmail.com>
2023-12-02 12:11:45 +0900

    ✨ feat: #3 shadcnコンポーネントをstorybookに追加

--- src/components/ui/button.stories.tsx
+++ src/components/ui/button.stories.tsx
@@ -15,6 +15,9 @@ const meta: Meta<typeof Button> = {
         type: 'select',
         options: ['default', 'sm', 'lg', 'icon']
       }
+    },
+    className: {
+      control: 'text'
     }
   }
 }
@@ -26,5 +29,82 @@ type Story = StoryObj<typeof Button>
 export const Base: Story = {
   args: {
     children: 'Base'
+  }
+}
+
+export const Destructive: Story = {
+  args: {
+    children: 'Destructive',
+    variant: 'destructive'
+  }
+}
+
+export const Outline: Story = {
+  args: {
+    children: 'Outline',
+    variant: 'outline'
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary',
+    variant: 'secondary'
+  }
+}
+
+export const Ghost: Story = {
+  args: {
+    children: 'Ghost',
+    variant: 'ghost'
+  }
+}
+
+export const Link: Story = {
+  args: {
+    children: 'Link',
+    variant: 'link'
+  }
+}
+
+export const Small: Story = {
+  args: {
+    children: 'Small',
+    size: 'sm'
+  }
+}
+
+export const Large: Story = {
+  args: {
+    children: 'Large',
+    size: 'lg'
+  }
+}
+
+export const Icon: Story = {
+  args: {
+    children: 'Icon',
+    size: 'icon'
+  }
+}
+
+export const Disabled: Story = {
+  args: {
+    children: 'Disabled',
+    disabled: true
+  }
+}
+
+export const AsChild: Story = {
+  args: {
+    children: 'AsChild',
+    asChild: true
+  }
+}
+
+export const CustomClass: Story = {
+  args: {
+    children: 'CustomClass',
+    className: 'text-red-500'
   }
 }
