b413842623c4c40029309c8000c63f8bb9c9a306
Fukuemon <Fukuemony029@gmail.com>
2023-12-10 15:48:55 +0900

    💄 style: #9 propsで管理するFlexBoxコンポーネント

--- src/app/(style)/flex/page.tsx
+++ src/app/(style)/flex/page.tsx
@@ -1,7 +1,9 @@
-import { SplitScreen } from './_conponets/Flex/SplitScreen/SplitScreen'
+import FlexBox from './_components/FlexBox/FlexBox'
+import { FlexColumn, FlexLayout } from './_components/FlexLayout/FlexLayout'
+import { DynamicSplitScreen, SplitScreen } from './_components/SplitScreen/SplitScreen'
 
 const LeftComponent = ({ name }: { name: string }) => {
-  return <h1 className=" bg-blue-100 text-center">{name}</h1>
+  return <h1 className="flex bg-blue-100 text-center h-36 items-center justify-center">{name}</h1>
 }
 
 const RightComponent = ({ message }: { message: string }) => {
@@ -9,9 +11,34 @@ const RightComponent = ({ message }: { message: string }) => {
 }
 export default function Flex() {
   return (
-    <SplitScreen leftWeight={2} rightWeight={3}>
-      <LeftComponent name="John" />
-      <RightComponent message="Hello" />
-    </SplitScreen>
+    <div>
+      <SplitScreen leftWeight={2} rightWeight={3}>
+        <LeftComponent name="John" />
+        <RightComponent message="Hello" />
+      </SplitScreen>
+      <div>
+        <DynamicSplitScreen weights={[1, 2, 3]}>
+          <LeftComponent name="John" />
+          <RightComponent message="Hello" />
+          <LeftComponent name="John" />
+        </DynamicSplitScreen>
+      </div>
+      <div>
+        <FlexLayout direction="column" gap="1px" padding="1px">
+          <FlexColumn flexSize="1 1 100px" margin="10px" padding="15px">
+            <LeftComponent name="John" />
+          </FlexColumn>
+          <FlexColumn flexSize="1 1 200px" margin="10px" padding="15px">
+            <RightComponent message="Hello" />
+          </FlexColumn>
+        </FlexLayout>
+      </div>
+      <div>
+        <FlexBox flexDirection="column" gap="2px" padding="2px">
+          <LeftComponent name="John" />
+          <RightComponent message="Hello" />
+        </FlexBox>
+      </div>
+    </div>
   )
 }
