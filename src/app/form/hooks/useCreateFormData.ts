9fdac5aa6943fe09e0a1de8619dc0d35e80b53bd
Fukuemon <Fukuemony029@gmail.com>
2023-12-27 15:23:00 +0900

    ✨ feat: #12 画像圧縮処理の追加

--- src/app/form/hooks/useCreateFormData.ts
+++ src/app/form/hooks/useCreateFormData.ts
@@ -1,5 +1,6 @@
 'use client'
 import { zodResolver } from '@hookform/resolvers/zod'
+import loadImage, { LoadImageOptions } from 'blueimp-load-image'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { useFileInput } from '../components/FileInput/FileInput.hooks'
@@ -35,13 +36,15 @@ export const useCreateFormData = () => {
     }
   })
 
-  const onSubmit = (data: PostType) => {
-    console.log(data)
+  const onSubmit = async (data: PostType) => {
+    // console.log(data)
     try {
       const formData = new FormData()
       formData.append('title', data.title)
       if (data.image) {
-        formData.append('image', data.image)
+        const compressedImage = await compressImage(data.image)
+        formData.append('image', compressedImage)
+        // formData.append('image', data.image)
       }
       if (data.model) {
         formData.append('model', data.model)
@@ -54,6 +57,44 @@ export const useCreateFormData = () => {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  // loadImage関数の型定義
+  type LoadImage = (
+    file: File | Blob | string,
+    callback: (canvas: HTMLCanvasElement) => void,
+    options?: LoadImageOptions
+  ) => void
+
+  // 画像圧縮処理
+  const compressImage = (file: File): Promise<Blob> => {
+    return new Promise((resolve, reject) => {
+      ;(loadImage as LoadImage)(
+        file,
+        (canvas: HTMLCanvasElement) => {
+          if (!canvas.toBlob) {
+            reject(new Error('お使いのブラウザは Canvas.toBlob メソッドをサポートしていません'))
+            return
+          }
+
+          canvas.toBlob(
+            (blob) => {
+              if (blob) {
+                resolve(blob)
+              } else {
+                reject(new Error('Image compression resulted in null Blob'))
+              }
+            },
+            file.type,
+            0.7
+          )
+        },
+        {
+          maxWidth: 1200,
+          canvas: true
+        }
+      )
+    })
   }
 
   return {
